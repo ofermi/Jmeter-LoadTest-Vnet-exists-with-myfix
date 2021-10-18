567f7572f69a312449ed716be1adc7c0ba64d383
עופר מירוני <oferm@cloudvalley.io>
2021-10-18 05:56:00 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ data "azurerm_container_registry" "jmeter_acr" {
   name                = var.JMETER_ACR_NAME
   resource_group_name = var.JMETER_ACR_RESOURCE_GROUP_NAME
 }
-data  "azurerm_subnet" "jmeter_subnet" {
- name                 = "jmetersubnet"
- resource_group_name  = "jmeter"
- virtual_network_name = "jmetervnet"
-}
+#data  "azurerm_subnet" "jmeter_subnet" {
+# name                 = "jmetersubnet"
+# resource_group_name  = "jmeter"
+# virtual_network_name = "jmetervnet"
+#}
 
 
  
@@ -36,24 +36,24 @@ resource "random_id" "random" {
 #}
 
 
-#resource "azurerm_subnet" "jmeter_subnet" {
-  #name                 = "${var.PREFIX}subnet1"
+resource "azurerm_subnet" "jmeter_subnet" {
+  name                 = "${var.PREFIX}subnet1"
   
- # resource_group_name  = "jmeter"
-  #virtual_network_name = "jmetervnet"
- # address_prefix       = var.SUBNET_ADDRESS_PREFIX
+  resource_group_name  = "jmeter"
+  virtual_network_name = "jmetervnet"
+  address_prefix       = var.SUBNET_ADDRESS_PREFIX
 
-  #delegation {
-   # name = "delegation"
+  delegation {
+    name = "delegation"
 
-   # service_delegation {
-    #  name    = "Microsoft.ContainerInstance/containerGroups"
-   #   actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-   # }
-  #}
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 
-#  service_endpoints = ["Microsoft.Storage"]
-#}
+  service_endpoints = ["Microsoft.Storage"]
+}
 
 resource "azurerm_network_profile" "jmeter_net_profile" {
   name                = "${var.PREFIX}netprofile"
